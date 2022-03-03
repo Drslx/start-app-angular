c683651905b3e024922dfcf66df120aea3b438fa
Drslx <drslxprogramador@gmail.com>
2022-03-02 22:35:17 -0300

    first commit develop - create the cart view

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+// Services
+import { CartService } from "../../service/cart.service";
+@Component({
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"],
+})
+export class CartComponent implements OnInit {
+  // propriedade para armazenar os produtos no carrinho.
+  items = this.cartService.getItems();
+
+  // Services injetado
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {}
+}
